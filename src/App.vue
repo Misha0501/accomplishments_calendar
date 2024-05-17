8bab3bb30c519ff212965cc0e67eba094596865b
misha0501 <misha.galenda@gmail.com>
2024-05-17 14:03:39 +0400

    replaced tailwindcss with Vuetify

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,16 @@ import AccomplishmentsCalendar from '@/components/AccomplishmentsCalendar.vue'
     <div class="">
       <AccomplishmentsCalendar/>
     </div>
+    <v-bottom-sheet>
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props" text="Click Me"></v-btn>
+      </template>
+
+      <v-card
+        title="Bottom Sheet"
+        text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, eos? Nulla aspernatur odio rem, culpa voluptatibus eius debitis dolorem perspiciatis asperiores sed consectetur praesentium! Delectus et iure maxime eaque exercitationem!"
+      ></v-card>
+    </v-bottom-sheet>
   </header>
 </template>
 
